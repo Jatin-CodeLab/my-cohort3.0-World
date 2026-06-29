8fcac40252d5f1039270e81001b5c05b447cc23d
Jatin Prajapati <jatin.prajapati.frontenddev@gmail.com>
2026-06-29 11:48:54 +0530

    Js Dom

---
+++ JS Prectices QuestionS/JavaScript - DOM Practice Questions/script.js
@@ -0,0 +1,33 @@
+console.log(`Hare Krishna`);
+
+// // //? Even or Odd Checker :
+// let checkNumberEvenOrOdd = (n) => {
+// 	if (n % 2 === 0) {
+// 		console.log("Even !");
+// 	} else {
+// 		console.log("Odd !");
+// 	}
+// };
+// checkNumberEvenOrOdd(34);
+
+// //? Greeting Generator :
+// let persnolInformationPrint = (name, age) => {
+// 	return console.log(`Your name is ${name}, and age is ${age}.`);
+// };
+// persnolInformationPrint("Krishna", 15);
+
+// //? Rectangle Area Calculator
+// let widht
+// let height
+// let checkerRectangleOfArea = (widht, height) => {
+// 	return console.log(widht * height);
+// };
+// checkerRectangleOfArea(34,54)
+
+
+// //? Private Counter with Closures
+// let count;
+// let ClosuresCounter = (n) => {
+//     let number = 34
+// }
+// ClosuresCounter(23)
