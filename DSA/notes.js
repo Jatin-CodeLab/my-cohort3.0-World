65e49475a7e6c8eb1d2b4539f1ef7a3963d312d0
Jatin Prajapati <jatin.prajapati.frontenddev@gmail.com>
2026-07-25 03:21:12 +0530

    DSA in variable

---
+++ DSA/notes.js
@@ -0,0 +1,32 @@
+console.log("--- notes.js file running...");
+//? DSA ?//
+
+//? Topic : 1 = variables :
+// Variables => var, let, const
+//>var me value ko reassign kar sakte hai, ye scope ko follow nahi karta, is me define ke pahele variable ko use kare ge to undefined aaye ga.
+// var a = 10;
+// a = 49 // yaha per value reassign ho gayi hai
+// console.log(a);
+
+//>let me value ko reassign kar sakte hai, ye scope ko follow karta hai, is me define ke pahele variable ko use nahi kar sakte, is me error aaye gi.
+// let a = 10;
+// a = 49 // yaha per value reassign ho gayi hai
+// console.log(a);
+
+//>const me value ko reassign nahi kar sakte, ye scope ko follow karta hai, is me define ke pahele variable ko use nahi kar sakte, is me error aaye gi
+// const a = 10;
+// a = 49 // yaha per value reassign nahi hogi or kakre ge to,TypeError: Assignment to constant variable.
+// console.log(a);
+
+//! hame hamesha let or const hi use kar na hai, kabhi bhi var nahi use kar na...
+
+//?=== === === === === === === === === === === === === === === === === === === ?//
+
+//? Topic : 2 : variable Rules :
+// string + string = string (Contenation)
+// string + integer = string (Contenation)
+// integer + integer = integer (arithmatic opration)
+
+//? Topic : 3
+//? Topic : 4
+//? Topic : 5
---
+++ DSA/topics/jsEss.js
@@ -0,0 +1,21 @@
+console.log("--- jsEss.js file running...");
+
+//? 1. Sum of two integer.
+// let a = 13, b = 7;
+// console.log(a+b);
+// console.log(typeof(a+b)); // 2 number sum = number
+
+// let a = 'krishna', b = 13;
+// console.log(a+b);
+// console.log(typeof(a+b)); // 1 string, 1 number sum = string
+
+// let a = 'krishna', b = " vrindavan";
+// console.log(a+b);
+// console.log(typeof(a+b)); // 2 string sum = string
+
+// ex :
+// let a = 1,
+// 	b = 4;
+// console.log("This two number is " + a + " and " + b + " sum :" + a + b); //this is worng !
+// console.log("This two number is " + a + " and " + b + " sum :" + (a + b)); //this is right, but not all !
+// console.log(`This two number is ${a}and ${b}sum : ${a+b}`); //this is right !
